762078479e6d32b758a0864262b91156ed36bbeb
Jonas <jonasper19@gmail.com>
2025-07-16 10:17:29 -0300

    routes-update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ScreenLogin from "./pages/login";
 import Home from "./pages/home";
 import Configuration from "./pages/configuration";
@@ -9,8 +9,10 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/" element={<Navigate to="/login" replace />} />
+
 				<Route path="/login" element={<ScreenLogin />} />
+				<Route path="/home" element={<Home />} />
 				<Route path="/configuration" element={<Configuration />} />
 				<Route path="/scrap-data" element={<ScrapData />} />
 				<Route path="/dashboard" element={<Dashboard />} />
