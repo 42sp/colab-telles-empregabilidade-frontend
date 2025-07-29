826bfdc22ef12ce86764edeb645a7f4966638989
Jonas <jonasper19@gmail.com>
2025-07-29 10:05:55 -0300

    ok

--- src/pages/home/status/DrawTotals.tsx
+++ src/pages/home/status/DrawTotals.tsx
@@ -1,10 +1,16 @@
-export function DrawTotals(props: any) {
+import type { StatusType } from "../types";
+
+interface DrawTotalsProps {
+	status: StatusType[];
+}
+
+export function DrawTotals({ status }: DrawTotalsProps) {
 	const background: string =
 		" flex flex-col flex-1 w-full bg-white rounded-md p-4 gap-4 border border-b border-gray-200 flex-shrink-0";
 
 	return (
 		<div className="flex flex-wrap gap-4">
-			{props.status.map(({ label, value }) => {
+			{status.map(({ label, value }) => {
 				const displayValue =
 					label === "Salário Médio"
 						? value.toLocaleString("pt-BR", {
