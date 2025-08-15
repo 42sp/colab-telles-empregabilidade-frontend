95ca85aa556ffbf25314368d66400265f0c65f42
BrianFs <brianfsilvacode@outlook.com>
2025-08-14 20:59:10 -0400

    incluir sheet bd_geral em tabela students

--- src/pages/import/index.tsx
+++ src/pages/import/index.tsx
@@ -8,28 +8,54 @@ import { FadeInOnScroll } from "@/components/utils/FadeInOnScroll";
 import { useServices } from "@/hooks/useServices";
 import type { fileProps } from "@/types/requests/interfaces/fileProps";
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 const Configuration = () => {
 	const [files, setFiles] = useState<fileProps[]>([]);
 
-	const services = useServices();
+	const $services = useServices();
 
 	const uploadFiles = async () => {
-		const response = await services.postImportFiles({ files });
+		if (files.length === 0) {
+			toast.info("Nenhum arquivo selecionado");
+			return;
+		}
 
-		response.forEach(res => {
-			if ([200, 201].includes(res.status)) {
-				const message = res.data.message;
-
-				files.forEach(file => {
-					if (message.includes(file.name)) {
-						file.status = "Sucesso";
+		for (const file of files) {
+			try {
+				const response = await toast.promise(
+					$services.postImportFiles({ file }),
+					{
+						pending: `Exportando arquivo ${file.name}...`,
+						success: `Arquivo ${file.name} importado com sucesso 👌`,
+						error: `Erro ao importar o arquivo ${file.name} 🤯`,
 					}
-				});
+				);
 
-				setFiles([...files]);
+				if ([200, 201].includes(response.status)) {
+					file.status = "Sucesso";
+					setFiles([...files]);
+				}
+			} catch (error) {
+				console.error(error);
+				if (
+					error instanceof Error &&
+					(
+						error as {
+							response?: {
+								data?: {
+									message?: string;
+								};
+							};
+						}
+					)?.response?.data?.message?.includes("duplicate key value")
+				) {
+					toast.info(
+						`O arquivo ${file.name} contém estudantes com IDs que já foram importados anteriormente.`
+					);
+				}
 			}
-		});
+		}
 	};
 
 	return (
