a00fcaee1ab47c8142a038dd10020d8cd0c2409f
Jonas <jonasper19@gmail.com>
2025-08-18 09:34:55 -0300

    ok

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,8 @@
 import { StudentsList } from "../../components/home/studentsList";
-import { useSidebar } from "../../contexts/SidebarContext"; // Ajuste o caminho conforme necessário
 
 function Home() {
-	const { isCollapsed } = useSidebar();
-
 	return (
-		<div
-			className={`bg-slate-50 contain-layout transition-all duration-300 ${
-				isCollapsed ? "ml-0 lg:ml-16" : "ml-0 lg:ml-50"
-			}`}
-		>
+		<div className="contain-layout">
 			<div className="container mx-auto">
 				<div className="w-full">
 					<StudentsList />
